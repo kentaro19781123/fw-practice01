4c9c395ca8ed57cc72e39097a4fb6af330aab02a
suzuki kentaro <suzuki-ke@gnavi.co.jp>
2016-09-15 19:24:34 +0900

    practice01 01作成

--- src/js/common/script01.js
+++ src/js/common/script01.js
@@ -5,21 +5,25 @@
  *****------------------------------------------------------------------*/
 (function () {
   'use strict';
-  
+
   // shortcut
   var doc = document;
 
   /**
    * jquery
    *--------------------------------------------------*/
-  
-  
+
+
   // 〜 practice01の課題をここに記述 〜
-  
-  
-  
+  var buttonAlert = function(targetSel){
+    var buttonText = targetSel.text();
+    alert(buttonText);
+  }
+
+  $('.js-button-jquery button').on('click',function(){
+    var targetSel = $(this);
+    buttonAlert(targetSel);
+  });
+
+
 })();
-
-
-
-
